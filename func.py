536022d04d97e1689a9f12a9dcdf14d3faafea70
Niraj <nirajbjk@gmail.com>
2017-11-22 14:51:15 -0500

    Corrected the GPIO pins for movements.

--- func.py
+++ func.py
@@ -13,9 +13,9 @@ def forward(tf):
 
 
     print 'Moving Forward...'
-    gpio.output(11, True)
+    gpio.output(7, True)
     gpio.output(13, True)
-    gpio.output(7, False)  #New Line
+    gpio.output(11, False)  #New Line
     gpio.output(15, False) #New Line
 
     time.sleep(tf)
@@ -24,9 +24,9 @@ def forward(tf):
 def reverse(tf):
 
     print 'Moving Backward...'
-    gpio.output(7, True)
+    gpio.output(11, True)
     gpio.output(15, True)
-    gpio.output(11, False) #New Line
+    gpio.output(7, False) #New Line
     gpio.output(13, False) #New Line
 
     time.sleep(tf)
@@ -36,10 +36,10 @@ def turn_left(tf):
 
 
     print 'Turning Left...'
-    gpio.output(11, False)
-    gpio.output(7, False)  #New Line
-    gpio.output(13, True) #New Line
-    gpio.output(15, False) #New Line
+    gpio.output(7, True)
+    gpio.output(15, False)  #New Line
+    gpio.output(13, False) #New Line
+    gpio.output(11, False) #New Line
     time.sleep(tf)
     gpio.cleanup()
 
@@ -47,9 +47,9 @@ def turn_right(tf):
 
 
     print 'Turning Right...'
-    gpio.output(13, False)
-    gpio.output(7, False)  #New Line
-    gpio.output(11, True) #New Line
+    gpio.output(11, True)
+    gpio.output(13, False)  #New Line
+    gpio.output(7, False) #New Line
     gpio.output(15, False) #New Line
 
     time.sleep(tf)
@@ -60,10 +60,10 @@ def pivot_left(tf):
 
 
     print 'Pivoting Left...'
-    gpio.output(11, False)
-    gpio.output(15, False)
-    gpio.output(7, True) #New Line
-    gpio.output(13, True) #New Line
+    gpio.output(7, True)
+    gpio.output(15, True)
+    gpio.output(13, False) #New Line
+    gpio.output(11, False) #New Line
     time.sleep(tf)
     gpio.cleanup()
 
@@ -72,9 +72,9 @@ def pivot_right(tf):
 
 
     print 'Pivoting Right...'
-    gpio.output(7, False)
-    gpio.output(13, False)
-    gpio.output(11, True) #New Line
-    gpio.output(15, True) #New Line
+    gpio.output(13, True)
+    gpio.output(11, True)
+    gpio.output(7, False) #New Line
+    gpio.output(15, False) #New Line
     time.sleep(tf)
     gpio.cleanup()
